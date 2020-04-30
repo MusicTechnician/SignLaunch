8813aa7c0aca9358e5dd847beaf1f2c5ae3c6a17
BuildTools <unconfigured@null.spigotmc.org>
2020-04-30 20:20:30 +0100

    Tidied code up.

--- SignLaunch/src/uk/co/MusicTechnician/SignLaunch/Events.java
+++ SignLaunch/src/uk/co/MusicTechnician/SignLaunch/Events.java
@@ -1,11 +1,13 @@
 package uk.co.MusicTechnician.SignLaunch;
 
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
+import org.bukkit.block.data.AnaloguePowerable;
+import org.bukkit.block.data.Powerable;
+import org.bukkit.block.data.type.WallSign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -27,41 +29,28 @@ public class Events implements Listener{
 		//ub = Underneath Block, Built this location to be able to check 2 blocks below 
 		Block ub = new Location(world, x, y - 2, z).getBlock();
 		
+		
 		//checks if pressure plate + sign combo		
-		if(!(e.getAction() == Action.LEFT_CLICK_BLOCK)) {
-		if(b.getType() == Material.STONE_PRESSURE_PLATE ||
-			b.getType() == Material.OAK_PRESSURE_PLATE ||
-			b.getType() == Material.SPRUCE_PRESSURE_PLATE ||
-			b.getType() == Material.BIRCH_PRESSURE_PLATE ||
-			b.getType() == Material.JUNGLE_PRESSURE_PLATE ||
-			b.getType() == Material.ACACIA_PRESSURE_PLATE ||
-			b.getType() == Material.DARK_OAK_PRESSURE_PLATE ||
-			b.getType() == Material.LIGHT_WEIGHTED_PRESSURE_PLATE ||
-			b.getType() == Material.HEAVY_WEIGHTED_PRESSURE_PLATE){
-			if(ub.getType() == Material.ACACIA_SIGN || 
-					ub.getType() == Material.ACACIA_WALL_SIGN ||
-					ub.getType() == Material.BIRCH_SIGN || 
-					ub.getType() == Material.BIRCH_WALL_SIGN ||
-					ub.getType() == Material.DARK_OAK_SIGN || 
-					ub.getType() == Material.DARK_OAK_WALL_SIGN ||
-					ub.getType() == Material.JUNGLE_SIGN|| 
-					ub.getType() == Material.JUNGLE_WALL_SIGN ||
-					ub.getType() == Material.OAK_SIGN || 
-					ub.getType() == Material.OAK_WALL_SIGN ||
-					ub.getType() == Material.SPRUCE_SIGN|| 
-					ub.getType() == Material.SPRUCE_WALL_SIGN){
+		if((e.getAction() != Action.LEFT_CLICK_BLOCK && e.getAction() != Action.RIGHT_CLICK_BLOCK)) {
+		if(b.getBlockData() instanceof Powerable ||
+			b.getBlockData() instanceof AnaloguePowerable){
+			if(ub.getState() instanceof Sign || 
+				ub.getBlockData() instanceof WallSign){
 				Sign sign = (Sign)ub.getState();
+				//checks for correct top line
 					if(sign.getLine(0).equals("[Launch]")) {
 						
+						//grabs remaining sign lines and makes sure they're floats
+						try {
 						float sx = Float.parseFloat(sign.getLine(1));
 						float sy = Float.parseFloat(sign.getLine(2));
 						float sz = Float.parseFloat(sign.getLine(3));
-						
-						
-					
-				//Launch Player
-				p.setVelocity(p.getLocation().getDirection().setX(sx).setY(sy).setZ(sz));
-				p.playSound(p.getLocation(), Sound.BLOCK_END_PORTAL_FRAME_FILL, 1F, 1F);
+						//Launch Player
+						p.setVelocity(p.getLocation().getDirection().setX(sx).setY(sy).setZ(sz));
+						p.playSound(p.getLocation(), Sound.BLOCK_END_PORTAL_FRAME_FILL, 1F, 1F);						
+						} catch (NumberFormatException except) {
+							//ignore formatting errors
+						}
 					}
 				}
 			}
